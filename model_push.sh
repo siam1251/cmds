a43cc8d360400fb140f3dd0947e16c93aa459a9c
Sayem Mohammad Siam <sayem.mohammad.siam@huawei.com>
2019-07-25 15:28:15 +0000

    updated

---
+++ model_push.sh
@@ -0,0 +1,4 @@
+adb shell rm /product/region_comm/china/aimodel/cv/kirin980/cv_videoface_face_feature_detection_batch4.cambricon
+adb push face /product/region_comm/china/aimodel/cv/kirin980/cv_videoface_face_feature_detection_batch4.cambricon
+adb shell rm /product/region_comm/china/aimodel/cv/kirin980/cv_videoface_ssd_core_detection_graph.cambricon
+adb push core /product/region_comm/china/aimodel/cv/kirin980/cv_videoface_ssd_core_detection_graph.cambricon
